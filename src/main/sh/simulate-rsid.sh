35ea74160d7407f2598a93554b587448dce8dbba
marpiech <piechota.marcin@gmail.com>
2021-08-12 08:51:35 +0000

    added symlink to resources

--- src/main/sh/simulate-rsid.sh
+++ src/main/sh/simulate-rsid.sh
@@ -1,4 +1,21 @@
 RSID=$1
 
+
+SIMULATION_BED="${PROJECT_DIR}resources/miscellaneous/simulated-samples/bed_files/cyps_2mln.bed"
+INPUT_TEMPLATE="${PROJECT_DIR}resources/miscellaneous/simulated-samples/samples/pgx-test/create-inputs/template-inputs.json"
 CHROMOSOME="chr22"
-SAMPLE
+SAMPLE_NAME="$GENE-star-$STAR"
+SIMULATION_VCF_GZ="$OUTPUT_PATH/$GENE-star-$STAR-and-cyps.vcf.gz"
+COVERAGE="30"
+
+
+sed -e "s#\$SIMULATION_BED#$SIMULATION_BED#" \
+-e "s#\$CHROMOSOME#$CHROMOSOME#" \
+-e "s#\$SAMPLE_NAME#$SAMPLE_NAME#" \
+-e "s#\$SIMULATION_VCF_GZ#$SIMULATION_VCF_GZ#" \
+-e "s#\$COVERAGE#$COVERAGE#" \
+$INPUT_TEMPLATE > $OUTPUT_PATH/$GENE-star-$STAR-simulate-neat-input.json
+
+echo "$OUTPUT_PATH/$GENE-star-$STAR-simulate-neat-input.json"
+cat "$OUTPUT_PATH/$GENE-star-$STAR-simulate-neat-input.json"
+
